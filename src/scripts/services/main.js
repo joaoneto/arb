da0d5bfbb6d5eea56bce239de4240d73e957dd1e
João Neto <joaopintoneto@gmail.com>
2013-10-20 21:51:54 -0200

    added notifications service

--- src/scripts/services/main.js
+++ src/scripts/services/main.js
@@ -2,12 +2,14 @@ define([
   'angular',
   'ngResource',
   'services/Auth',
-  'services/Page'
+  'services/Page',
+  'services/Notifications'
 ], function () {
   return angular
     .module('arb.services', [
       'ngResource',
       'arb.services.Auth',
-      'arb.services.Page'
+      'arb.services.Page',
+      'arb.services.Notifications'
     ]);
 });
