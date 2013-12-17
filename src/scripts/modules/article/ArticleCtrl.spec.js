92bcf32722079544c67d15e9e30c1aa0a5363ec0
Gabriel Mancini <gabriel.mancini@gmail.com>
2013-12-17 20:20:29 -0200

    typo

--- src/scripts/modules/article/ArticleCtrl.spec.js
+++ src/scripts/modules/article/ArticleCtrl.spec.js
@@ -1,14 +1,14 @@
 //The tests
-describe('<Unit Test>', function() {
-  describe('Article:', function() {
+describe('<Unit Test> ArticleCtrl', function() {
+
+  beforeEach(module('arb'));
+
+  describe('Internal methods', function() {
 
     var $scope, ArticleCtrl;
 
-    beforeEach(module('arb'));
-
     beforeEach(
       inject(function ($controller, $rootScope, $state, ArbRest ) {
-
         $scope = $rootScope.$new();
         ArticleCtrl = $controller('ArticleCtrl', {
           $scope: $scope,
@@ -16,15 +16,12 @@ describe('<Unit Test>', function() {
         });
         
         $state.transitionTo('article');
-
       })
     );
 
     afterEach(
       inject(function ($rootScope) {
-
         $rootScope.$digest();
-
       })
     );
 
@@ -36,50 +33,41 @@ describe('<Unit Test>', function() {
 
       beforeEach(
         inject(function ($httpBackend, conf) {
-          
           var baseUrl = conf.getApiUrl();
           $httpBackend.expectPOST( baseUrl + '/article').respond();
-
         })
       );
 
       afterEach(
         inject(function ($httpBackend) {
-
           $httpBackend.flush();
-
         })
       );
 
       it('should be able to save without problems', 
         inject(function ($rootScope, $state, conf) {
-
           $state.transitionTo('article.create');
 
           $scope.title = 'title of article';
           $scope.contents = 'contents of article';
 
           ArticleCtrl.create($state, $scope);
-
         })
       );
 
-
-
       it('should be able to show an error when try to save without title', 
         inject(function ($rootScope, $state, conf) {
-
           $state.transitionTo('article.create');
 
           $scope.title = 'title of article';
           $scope.contents = 'contents of article';
 
           ArticleCtrl.create($state, $scope);
-
         })
       );
 
     });
 
   });
+
 });
